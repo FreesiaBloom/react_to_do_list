0940ac7dba1c55620f7a5e0c4f8a1b3363709992
jasinaga <agata.jasinska@tieto.com>
2020-01-08 14:18:26 +0100

    adding scss files

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import './TodoComponents.css'
+import './TodoComponents.scss'
 
 export class Todoitem extends Component {
     getStyle = () => {
---
+++ src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './layoutStyling.scss'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>To do list</h1>
+        </header>
+    )
+}
